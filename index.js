455df413d8c47c8dac6d85e70d1614b5deca98d7
Marc Backes <hello@marc.dev>
2021-08-20 13:27:39 +0200

    🔥 initial commit

---
+++ index.js
@@ -0,0 +1,73 @@
+const yargs = require('yargs/yargs')
+const { hideBin } = require('yargs/helpers')
+const git = require('simple-git')()
+const { red } = require('chalk')
+
+const {
+    mergingMessage,
+    checkedOutMessage,
+    checkoutErrorMessage,
+    conflictMessage,
+    mergeMessage,
+    noBranchesMessage,
+    uncommittedMessage,
+    completedMessage,
+} = require('./texts')
+
+const checkout = async branch => {
+    try {
+        await git.checkout(branch)
+        checkedOutMessage(branch)
+        return true
+    } catch (error) {
+        checkoutErrorMessage(branch)
+        return false
+    }
+}
+
+const merge = async (current, branch) => {
+    try {
+        const mergeSummary = await git.mergeFromTo(current, branch, {
+            '--no-commit': true,
+            '--no-ff': true,
+        })
+        mergeMessage(current, branch, mergeSummary)
+        return true
+    } catch (error) {
+        if (error.git?.conflicts?.length) {
+            await git.merge({ '--abort': true })
+            conflictMessage(error.git.conflicts.length, branch)
+        } else {
+            console.log(red(error.message))
+        }
+        return false
+    }
+}
+
+const run = async () => {
+    try {
+        const branches = yargs(hideBin(process.argv)).argv._
+        const { current, modified } = await git.status()
+
+        if (modified.length) return uncommittedMessage(modified)
+        if (!branches.length) return noBranchesMessage()
+        let mergeErrors = 0
+        let count = 0
+        for (branch of branches) {
+            count += 1
+            mergingMessage(current, branch, count, branches.length)
+            const checkoutResult = await checkout(branch)
+            if (checkoutResult) {
+                const mergeResult = await merge(current, branch)
+                if (mergeResult) mergeErrors += 1
+            } else {
+                mergeErrors += 1
+            }
+            console.log('')
+        }
+        completedMessage(branches, mergeErrors)
+    } catch (error) {
+        console.log(red(error.message))
+    }
+}
+run()
---
+++ texts.js
@@ -0,0 +1,85 @@
+const { red, yellow, green, blue, bold } = require('chalk')
+
+const log = (message, type = 'info') => {
+    if (type === 'error') console.log(red(message))
+    else if (type === 'warning') console.log(yellow(message))
+    else if (type === 'success') console.log(green(message))
+    else console.log(blue(message))
+}
+
+// -------------------------------------------------
+
+const mergingMessage = (current, branch, count, total) => {
+    console.log(
+        `➜ Merging ${blue(current)} into ${blue(branch)} (${count}/${total})`,
+    )
+}
+// -------------------------------------------------
+const checkedOutMessage = branch => {
+    console.log(`  • Checked out ${blue(branch)}`)
+}
+
+const checkoutErrorMessage = branch => {
+    log(`  • Branch '${branch}' could not be checked out`, 'error')
+}
+// -------------------------------------------------
+const uncommittedMessage = modified => {
+    log(
+        'Please commit your changes in the following files before merging:',
+        'warning',
+    )
+    modified.forEach(file => log(`  - ${file}`, 'warning'))
+}
+
+const noBranchesMessage = () => {
+    log(`✘ No branches specified`, 'error')
+}
+
+// -------------------------------------------------
+const changesText = changes => {
+    if (changes === 1) return '1 change'
+    else return `${changes} changes`
+}
+
+const insertionsText = insertions => green(`${insertions}+`)
+const deletionsText = deletions => red(`${deletions}-`)
+const mergeMessage = (current, branch, { summary }) => {
+    const mergeLine = `${green('  • Merge successful!')}`
+    const changeLine = `${changesText(summary.changes)} (${insertionsText(
+        summary.insertions,
+    )}/${deletionsText(summary.deletions)})`
+    console.log(`${mergeLine} ${changeLine}`)
+}
+
+// -------------------------------------------------
+const conflictMessage = (conflicts, branch) => {
+    const conflictText =
+        conflicts === 1 ? '1 conflict' : `${conflicts} conflicts`
+    console.log(red(`  • ${conflictText} found! `) + `(Merge aborted)`)
+}
+
+// -------------------------------------------------
+const completedMessage = (branches, mergeErrors) => {
+    log(
+        `✔ ${branches.length - mergeErrors}/${
+            branches.length
+        } merges successful!`,
+        'success',
+    )
+    if (mergeErrors !== branches.length)
+        log(
+            `✘ ${mergeErrors}/${branches.length} branches could not be merged`,
+            'error',
+        )
+}
+
+module.exports = {
+    mergingMessage,
+    checkedOutMessage,
+    checkoutErrorMessage,
+    uncommittedMessage,
+    noBranchesMessage,
+    mergeMessage,
+    conflictMessage,
+    completedMessage,
+}
